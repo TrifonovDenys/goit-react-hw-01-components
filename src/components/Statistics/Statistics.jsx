745a262e39c6948d594d55e7980fba35c3447615
Trifonov Denys <trifonowden123@gmail.com>
2023-07-04 00:05:20 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,13 @@
 import { StatItem } from "./Stat-item.jsx"
 import css from './Statistics.module.css'
 import PropTypes from "prop-types";
-export const Statistics = ({stats, title}) => {
-  return <section className={css.statistics}>
-    <h2 className={css.title}>{title}</h2>
-
+export const Statistics = ({title, stats}) => {
+  return (<section className={css.statistics}>
+    {title && <h2 className={css.title}>{title}</h2>}
     <ul className={css.statlist}>
       <StatItem stats={stats} />
     </ul>
-  </section>
+  </section>)
 }
 
 Statistics.propTypes = {
