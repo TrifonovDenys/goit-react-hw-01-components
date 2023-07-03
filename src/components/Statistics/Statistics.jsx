4b96924c58267e6fdd763d0af5e42a06c733e7fc
Trifonov Denys <trifonowden123@gmail.com>
2023-07-03 23:54:55 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,16 @@
 import { StatItem } from "./Stat-item.jsx"
 import css from './Statistics.module.css'
-
-export const Statistics = ({stats}) => {
+import PropTypes from "prop-types";
+export const Statistics = ({stats, title}) => {
   return <section className={css.statistics}>
-    <h2 className={css.title}>Upload stats</h2>
+    <h2 className={css.title}>{title}</h2>
 
     <ul className={css.statlist}>
       <StatItem stats={stats} />
     </ul>
   </section>
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string.isRequired,
 }
